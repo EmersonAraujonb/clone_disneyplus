727394eac9709c093461a8dd9856e74bc0427761
Emerson Araújo <emersonsantos0302@gmail.com>
2025-12-05 20:37:04 -0300

    criando o hero e importando a fonte Avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,49 @@
-const Gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+import gulp from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
 
-function styles() {
-    return Gulp.src('./src/styles/*.scss')
-        .pipe(sass({ outputStyle: 'compressed' }))
-        .pipe(Gulp.dest('./dist/css'));
+import imagemin, {
+    gifsicle,
+    mozjpeg,
+    optipng,
+    svgo
+} from 'gulp-imagemin';
+
+// cria o compilador de sass corretamente
+const sass = gulpSass(dartSass);
+
+// ------------------------------
+// STYLES
+// ------------------------------
+export function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    Gulp.watch('./src/styles/*.scss', Gulp.parallel(styles));
+// ------------------------------
+// IMAGES
+// ------------------------------
+export function images() {
+    return gulp.src('src/images/**/*.{png,jpg,jpeg,gif,svg}')
+        .pipe(imagemin([
+            gifsicle({ interlaced: true }),
+            mozjpeg({ quality: 80 }),
+            optipng({ optimizationLevel: 5 }),
+            svgo()
+        ]))
+        .pipe(gulp.dest('./dist/images'));
 }
+
+// ------------------------------
+// WATCH
+// ------------------------------
+export function watch() {
+    gulp.watch('./src/styles/*.scss', styles);
+    gulp.watch('./src/images/**/*.{png,jpg,jpeg,gif,svg}', images);
+}
+
+// ------------------------------
+// DEFAULT
+// ------------------------------
+export default gulp.parallel(styles, images);
